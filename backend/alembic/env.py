dad7cdee939868518d7c009333a19324eddcea18
Ivan Vereshchagin <iveresagin932@gmail.com>
2026-03-16 18:39:33 +0300

    fix: чиню autogenerate для миграций

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -7,6 +7,8 @@ from alembic import context
 
 from app.database import Base
 
+from app.models import user, subscription, payment
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
