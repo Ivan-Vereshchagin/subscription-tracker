116da9fe4e5afb127844f2c82fe75851d3a69aaf
Ivan Vereshchagin <iveresagin932@gmail.com>
2026-04-25 16:43:57 +0300

    fix: исправляю ошибки workflow

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -6,12 +6,11 @@ from sqlalchemy import pool
 from alembic import context
 
 from app.database import Base
-
 from app.models import user, subscription, payment
+from app.config import settings
 
-# this is the Alembic Config object, which provides
-# access to the values within the .ini file in use.
 config = context.config
+config.set_main_option("sqlalchemy.url", settings.database_url)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
